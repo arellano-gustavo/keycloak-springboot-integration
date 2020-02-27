a8fd80a95bf0710a1fbd8ea48585d30bdb8d5cc2
garellano <garellano@localhost>
2020-02-26 23:38:29 -0600

    Se integra el front de VueJS como dependencia Maven

--- src/main/java/com/example/config/CustomInterceptor.java
+++ src/main/java/com/example/config/CustomInterceptor.java
@@ -1,34 +1,55 @@
 package com.example.config;
 
+import java.util.Collection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class CustomInterceptor implements HandlerInterceptor {
-	Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
+	private Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
 	@Override
-	public void afterCompletion(HttpServletRequest request,
-			HttpServletResponse response, Object object, Exception arg3)
-			throws Exception {
+	public void afterCompletion(
+			HttpServletRequest request,
+			HttpServletResponse response, 
+			Object obj, 
+			Exception e) {
+		try {
+			Collection<? extends GrantedAuthority> sch = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+			Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>)sch;
+			for(SimpleGrantedAuthority sfa : authorities) {
+				log.info("---->"+sfa.getAuthority());
+			}
+		} catch (Exception ex) {
+			log.info("Current user is null");
+		}
 		log.info("goose: Request is complete");
 	}
 
 	@Override
-	public void postHandle(HttpServletRequest request,
-			HttpServletResponse response, Object object, ModelAndView model)
+	public void postHandle(
+			HttpServletRequest request,
+			HttpServletResponse response, 
+			Object obj, 
+			ModelAndView model)
 			throws Exception {
 		log.info("goose: Handler execution is complete");
 	}
 
 	@Override
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object object) throws Exception {
+	public boolean preHandle(
+			HttpServletRequest request,
+			HttpServletResponse response, 
+			Object obj) throws Exception {
 		log.info("goose: Before Handler execution");
 		return true;
 	}
---
+++ src/main/java/com/example/config/WebMvcConfig.java
@@ -0,0 +1,41 @@
+package com.example.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(WebMvcConfig.class);
+			
+	@Autowired
+	private CustomInterceptor customInterceptor;
+
+	
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      logger.info("Setting up handlers for VueJS Locations");
+      registry
+          .addResourceHandler("index.html")
+          .addResourceLocations("classpath:/dist/index.html");
+      registry
+          .addResourceHandler("/js/**")
+          .addResourceLocations("classpath:/dist/js/");
+      registry
+      .addResourceHandler("/css/**")
+      .addResourceLocations("classpath:/dist/css/");
+
+    }
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(customInterceptor);
+	}
+}
